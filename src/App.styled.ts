3fd710266cc5c63033daef2257ce566ca69f3ab2
ebrahemsaleh <salehebrahem487@gmail.com>
2021-02-14 10:25:44 +0200

    style component app

---
+++ src/App.styled.ts
@@ -0,0 +1,16 @@
+  import styled from 'styled-components';
+  import IconButton from '@material-ui/core/IconButton';
+
+  export const Wrapper = styled.section`  
+  margin: 40px;
+  `;
+
+
+  export const StyledButton = styled(IconButton)`
+  position:fixed;
+  z-index:100;
+  right:6px;
+  top:5px
+  `
+
+  
